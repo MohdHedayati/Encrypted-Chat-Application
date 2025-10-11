990f33f16964e1b760154bc55eb09c88af8a8be2
Mohammed <hedayatimohd@gmail.com>
2025-10-11 20:45:11 +0530

    Fixing problems

--- chat-server/src/main/java/com/example/chatserver/config/WebSocketConfig.java
+++ chat-server/src/main/java/com/example/chatserver/config/WebSocketConfig.java
@@ -14,7 +14,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-chat")
-                .setAllowedOriginPatterns("https://encrypted-chat-application.vercel.app")
+                .setAllowedOriginPatterns("*")
                 .addInterceptors(new UsernameHandshakeInterceptor())
                 .setHandshakeHandler(new UsernameHandshakeInterceptor.UsernameHandshakeHandler())
                 .withSockJS();
